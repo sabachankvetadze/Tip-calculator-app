d8638af1e413012d33e666afdf1e01471a5917d4
saba chankvetadze <saba.chankvetadze@gmail.com>
2023-04-06 00:38:58 +0400

    add  commit

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const inputPay = document.querySelector(".button1");
+let inputPay = document.querySelector(".button1");
 const percent = document.querySelectorAll(".buttonall");
 const buttoncustom = document.querySelector(".custom");
 const inputpeople = document.querySelector(".quantity");
@@ -6,26 +6,40 @@ const tipAmount = document.querySelector(".dollar");
 const totalPerson = document.querySelector(".dollartwo");
 const buttonReset = document.querySelector(".buttonres")
 
+
 let percentAmount = 0;
 
 function percentAll(){
+    if (inputPay.value > 10000){
+        inputPay.value = 10000;
+       }
+       if (inputPay.value < 0){
+        inputPay.value = 0;
+       }
     const tip = inputPay.value * percentAmount / 100 / inputpeople.value;
    tipAmount.innerHTML = "$" + tip.toFixed(2);
    const total = (inputPay.value * percentAmount / 100  + +inputPay.value) / inputpeople.value;
    totalPerson.innerHTML = "$" + total.toFixed(2);
+  
    console.log(percentAmount)
 }
 
     percent.forEach((element) => {
     element.addEventListener("click", function(){
+        percent.forEach((button) =>{
+            button.classList.remove("active")
+        })
+        element.classList.add("active")
         percentAmount = element.value;
-        element.style.background = "#9FE8DF";
         percentAll();
     });
     });
   
     buttoncustom.addEventListener("click", function(){
         percentAmount = buttoncustom.value;
+        if(buttoncustom.value > 100){
+            buttoncustom.value = 100
+        }
         buttoncustom.style.border = "2px solid #26C2AE";
         percentAll()
     });
@@ -59,4 +73,6 @@ buttonReset.addEventListener("click", function(){
 
   
 
-
+ if(inputPay.value > 10000 ){
+    inputPay.value = 10000;
+ }
